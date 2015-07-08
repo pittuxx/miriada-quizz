4a662fb0fe7def6bdc8a9572bb9fd1899740d8dd
pittuxx <pittuxx@gmail.com>
2015-07-08 19:10:30 +0200

    Añadida página de créditos...

--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,9 @@ router.get('/', function(req, res) {
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
 
+/* GET credits page */
+router.get('/author',function(req,res){
+	res.render('author.ejs', {title: 'Créditos' });
+});
+
 module.exports = router;
